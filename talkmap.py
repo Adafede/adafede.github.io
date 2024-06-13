ae5b2978552e5715ad6aac89c9798e785cb17b5c
Adriano Rutz <adriano.rutz@hotmail.com>
2024-06-13 23:28:01 +0200

    up

--- talkmap.py
+++ talkmap.py
@@ -1,3 +1,5 @@
+
+
 # # Leaflet cluster map of talk locations
 #
 # (c) 2016-2017 R. Stuart Geiger, released under the MIT license
@@ -15,11 +17,13 @@ from geopy import Nominatim
 
 g = glob.glob("*.md")
 
-geocoder = Nominatim(user_agent="academic-pages")
+
+geocoder = Nominatim()
 location_dict = {}
 location = ""
 permalink = ""
 title = ""
+
 
 for file in g:
     with open(file, 'r') as f:
@@ -29,9 +33,15 @@ for file in g:
             lines_trim = lines[loc_start:]
             loc_end = lines_trim.find('"')
             location = lines_trim[:loc_end]
-
+                            
+           
         location_dict[location] = geocoder.geocode(location)
         print(location, "\n", location_dict[location])
 
+
 m = getorg.orgmap.create_map_obj()
 getorg.orgmap.output_html_cluster_map(location_dict, folder_name="../talkmap", hashed_usernames=False)
+
+
+
+
